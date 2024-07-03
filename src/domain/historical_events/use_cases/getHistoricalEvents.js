f067acaf85df40bb98cdb33af2743f9cc211f672
Laura Levraud <laura.levraud1@etu.univ-lorraine.fr>
2024-07-03 19:59:01 -0400

    Unit tests for historical events use case

--- src/domain/historical_events/use_cases/getHistoricalEvents.js
+++ src/domain/historical_events/use_cases/getHistoricalEvents.js
@@ -1,6 +1,36 @@
 import historicalEventsRepository from '../repository/historicalEventsRepository'
 
 exports.getHistoricalEventsByOcurrence = (ctx) => {
-    ctx.body = historicalEventsRepository.getHistoricalEvents(ctx.params.ocurrence)
+    try {
+        const ocurrence = ctx.params.ocurrence
+        if (ocurrence === undefined || ocurrence === null) {throw new Error('Occurrence not found')}
+
+        const eventos = historicalEventsRepository.getHistoricalEvents(ctx.params.ocurrence)
+        ctx.body = eventos
+        ctx.status = 200
+
+    } catch (error) {
+        switch (error.message) {
+            case 'Occurrence not found':
+                ctx.body = {"message": "Occurrence not found"}
+                ctx.status = 404
+                break;
+            
+            case 'El input debe ser ac o dc':
+                ctx.body = {"message": "El input debe ser ac o dc"}
+                ctx.status = 400
+                break;
+            
+            case 'Solo se aceptan caracteres no numéricos':
+                ctx.body = {"message": "Solo se aceptan caracteres no numéricos"}
+                ctx.status = 400
+                break;
+            
+            default:
+                ctx.body = {"message": "Internal server error"}
+                ctx.status = 500
+                break;
+        }
+    }
     return ctx
 }
---
+++ tests/domain/historical_events/historical_events_use_cases.unit.test.js
@@ -0,0 +1,176 @@
+import getHistoricalEventsByOcurrence from '../../../src/domain/historical_events/use_cases/getHistoricalEvents'
+
+describe('Test unitarios de el Historical Events Use cases', () => {
+
+    /**
+     * Test unitario para obtener eventos históricos de la era antes de cristo
+     * 1. Caso exitoso : el parametro usado es un string igual a 'ac', usando cada caso de case posible
+     */
+    test('Debería retornar debe devolver un status 200 y en el body, un arreglo con los eventos históricos de antes del cristo ordenados desde el más antiguo al más nuevo', () => {
+        const ocurrence = 'ac'
+        let ctx = getHistoricalEventsByOcurrence.getHistoricalEventsByOcurrence({params: {ocurrence: ocurrence}})
+        const body = ctx.body
+        const status = ctx.status
+
+        expect(status).toBe(200)
+        expect(body.length).toBeGreaterThan(0)
+        expect(body[0]).toEqual(getFirstEventBeforeChrist())
+        expect(Number(body[0].date)).toBeLessThanOrEqual(Number(body[1].date))
+
+        const ocurrence2 = 'AC'
+        ctx = getHistoricalEventsByOcurrence.getHistoricalEventsByOcurrence({params: {ocurrence: ocurrence2}})
+        const body2 = ctx.body
+        const status2 = ctx.status
+
+        expect(status2).toBe(200)
+        expect(body2.length).toBeGreaterThan(0)
+        expect(body2[0]).toEqual(getFirstEventBeforeChrist())
+        expect(Number(body2[0].date)).toBeLessThanOrEqual(Number(body2[1].date))
+
+        const ocurrence3 = 'Ac'
+        ctx = getHistoricalEventsByOcurrence.getHistoricalEventsByOcurrence({params: {ocurrence: ocurrence3}})
+        const body3 = ctx.body
+        const status3 = ctx.status
+
+        expect(status3).toBe(200)
+        expect(body3.length).toBeGreaterThan(0)
+        expect(body3[0]).toEqual(getFirstEventBeforeChrist())
+        expect(Number(body3[0].date)).toBeLessThanOrEqual(Number(body3[1].date))
+
+        const ocurrence4 = 'aC'
+        ctx = getHistoricalEventsByOcurrence.getHistoricalEventsByOcurrence({params: {ocurrence: ocurrence4}})
+        const body4 = ctx.body
+        const status4 = ctx.status
+
+        expect(status4).toBe(200)
+        expect(body4.length).toBeGreaterThan(0)
+        expect(body4[0]).toEqual(getFirstEventBeforeChrist())
+        expect(Number(body4[0].date)).toBeLessThanOrEqual(Number(body4[1].date))
+    })
+
+    /**
+     * Test unitario para obtener eventos históricos de la era después de cristo
+     * 1. Caso exitoso : el parametro usado es un string igual a 'dc', usando cada caso de case posible
+     */
+    test('Debería retornar eventos históricos de la era después de cristo, desde el más antiguo al más nuevo', () => {
+        const ocurrence = 'dc'
+        let ctx = getHistoricalEventsByOcurrence.getHistoricalEventsByOcurrence({params: {ocurrence: ocurrence}})
+        const body = ctx.body
+        const status = ctx.status
+
+        expect(status).toBe(200)
+        expect(body.length).toBeGreaterThan(0)
+        expect(body[0]).toEqual(getFirstEventAfterChrist())
+        expect(Number(body[0].date)).toBeLessThanOrEqual(Number(body[1].date))
+
+        const ocurrence2 = 'DC'
+        ctx = getHistoricalEventsByOcurrence.getHistoricalEventsByOcurrence({params: {ocurrence: ocurrence2}})
+        const body2 = ctx.body
+        const status2 = ctx.status
+
+        expect(status2).toBe(200)
+        expect(body2.length).toBeGreaterThan(0)
+        expect(body2[0]).toEqual(getFirstEventAfterChrist())
+        expect(Number(body2[0].date)).toBeLessThanOrEqual(Number(body2[1].date))
+
+        const ocurrence3 = 'Dc'
+        ctx = getHistoricalEventsByOcurrence.getHistoricalEventsByOcurrence({params: {ocurrence: ocurrence3}})
+        const body3 = ctx.body
+        const status3 = ctx.status
+
+        expect(status3).toBe(200)
+        expect(body3.length).toBeGreaterThan(0)
+        expect(body3[0]).toEqual(getFirstEventAfterChrist())
+        expect(Number(body3[0].date)).toBeLessThanOrEqual(Number(body3[1].date))
+
+        const ocurrence4 = 'dC'
+        ctx = getHistoricalEventsByOcurrence.getHistoricalEventsByOcurrence({params: {ocurrence: ocurrence4}})
+        const body4 = ctx.body
+        const status4 = ctx.status
+
+        expect(status4).toBe(200)
+        expect(body4.length).toBeGreaterThan(0)
+        expect(body4[0]).toEqual(getFirstEventAfterChrist())
+        expect(Number(body4[0].date)).toBeLessThanOrEqual(Number(body4[1].date))
+    })
+
+    /**
+     * Test unitario para obtener eventos históricos de la era antes o despues de cristo
+     * 2. Caso fallido : el parametro usado es un string de largo = 2, que contiene caracteres alfanuméricos o solo númericos
+     */
+    test('Debería retornar un error con el mensaje "Solo se aceptan caracteres no numéricos"', () => {
+        const ocurrence = 'a1'
+        let ctx = getHistoricalEventsByOcurrence.getHistoricalEventsByOcurrence({params: {ocurrence: ocurrence}})
+        const body = ctx.body
+        const status = ctx.status
+
+        expect(status).toBe(400)
+        expect(body).toEqual({"message": "Solo se aceptan caracteres no numéricos"})
+
+        const ocurrence2 = '1a'
+        ctx = getHistoricalEventsByOcurrence.getHistoricalEventsByOcurrence({params: {ocurrence: ocurrence2}})
+        const body2 = ctx.body
+        const status2 = ctx.status
+
+        expect(status2).toBe(400)
+        expect(body2).toEqual({"message": "Solo se aceptan caracteres no numéricos"})
+
+        const ocurrence3 = '11'
+        ctx = getHistoricalEventsByOcurrence.getHistoricalEventsByOcurrence({params: {ocurrence: ocurrence3}})
+        const body3 = ctx.body
+        const status3 = ctx.status
+
+        expect(status3).toBe(400)
+        expect(body3).toEqual({"message": "Solo se aceptan caracteres no numéricos"})
+    })
+
+    /**
+     * Test unitario para obtener eventos históricos de la era antes o despues de cristo
+     * 3. Caso fallido : el parametro usado es un string de largo != 2
+     */
+    test('Debería retornar un error con el mensaje "El input debe ser ac o dc"', () => {
+        const ocurrence = 'a'
+        const {body, status} = getHistoricalEventsByOcurrence.getHistoricalEventsByOcurrence({params: {ocurrence: ocurrence}})
+
+        expect(status).toBe(400)
+        expect(body).toEqual({"message": "El input debe ser ac o dc"})
+
+        const ocurrence2 = 'dcac'
+        const ctx = getHistoricalEventsByOcurrence.getHistoricalEventsByOcurrence({params: {ocurrence: ocurrence2}})
+        const body2 = ctx.body
+        const status2 = ctx.status
+
+        expect(status2).toBe(400)
+        expect(body2).toEqual({"message": "El input debe ser ac o dc"})
+    })
+
+    /**
+     * Test unitario para obtener eventos históricos de la era antes o despues de cristo
+     * 4. Caso fallido : el parametro no es definido
+     */
+    test('Debería retornar un error con el mensaje "Occurrence not found"', () => {
+        const {body, status} = getHistoricalEventsByOcurrence.getHistoricalEventsByOcurrence({params: {ocurrence: undefined}})
+
+        expect(status).toBe(404)
+        expect(body).toEqual({"message": "Occurrence not found"})
+    })
+})
+
+function getFirstEventBeforeChrist() {
+    return {
+        "date": "-299",
+        "description": "Los Samnitas, inician los preparativos de la Tercera guerra Samnita contra los romanos, reclutando tropas mercenarias de Galos y Sabinos y estableciendo alianzas con Etruscos y otros pueblos it\u00e1licos.",
+        "lang": "es",
+        "granularity": "year"
+    }
+}
+
+function getFirstEventAfterChrist() {
+    return {
+        "date": "1",
+        "description": "Fundación de Aksum (ciudad) en Etiopía.",
+        "lang": "es",
+        "category1": "África",
+        "granularity": "year"
+    }
+}
